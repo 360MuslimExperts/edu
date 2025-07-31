025767c4d947a2deafc2f599eb4be039e5c7110d
Ibtisam Shahid <ibtisam.shahid.kh@gmail.com>
2025-07-31 18:46:11 +0500

    .

--- downloads.php
+++ downloads.php
@@ -1,7 +1,7 @@
 <?php
 // --- SETUP AND HELPERS ---
 require_once __DIR__ . 'includes/config.php';
-require_once __DIR__ . 'includes/helpers.php';
+require_once __DIR__ . '/includes/helpers.php';
 
 // Security headers - MUST be before any output
 header('X-Frame-Options: SAMEORIGIN');
